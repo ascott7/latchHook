be43d95db41358c878b90ea931b4aa9aeae4cf14
Andrew Scott <ascott@hmc.edu>
2019-12-10 21:06:08 -0800

    Update color options

--- get_color_options.py
+++ get_color_options.py
@@ -1,6 +1,10 @@
 import cv2, sys, os
 import numpy as np
 
+# to get source images: go to main website and save-as, which will
+# download a resources folder containing all images
+# remove duplicates with find <dir> -regex ".*(1)\.jpg" -print0 | xargs -0 rm
+
 def chooseColor(img):
     rows, cols = img.shape[:2]
     gap = 5
@@ -16,4 +20,4 @@ if __name__ == "__main__":
             if ext == '.jpg':
                 img = cv2.imread(os.path.join(subdir, f))
                 colors[filename[8:]] = chooseColor(img).astype('uint8')
-    print colors
+    print(colors)
